90b7bbf2dde6517b493f7921be7bdbe9f0341f77
suraj <surajkaul15@gmail.com>
2022-06-13 21:30:06 +0530

    KICS path changes

--- demo/main.tf
+++ demo/main.tf
@@ -1,12 +1,12 @@
-module "Ec2" {
-  source = "git@github.com:Suraj-kaul/fresher-trainee---Ec2_instance.git"
-  ami = "ami-03e0b06f01d45a4eb"
-  instance_type = "t2.micro"
- 
+module "Ec2"{
 
-
+    source = "git@github.com:Suraj-kaul/fresher-trainee---Ec2_instance.git"
+    ami = "ami-03e0b06f01d45a4eb"
+    instance_type = "t2.micro"
 }
 
+   
+
 
  
 
@@ -15,10 +15,3 @@ module "Ec2" {
 
 
 
-module"s3_demo"{
-   ami = "ami-03e0b06f01d45a4eb"
-instance_type = "t2.micro"
-    
-    
-   
-}   
